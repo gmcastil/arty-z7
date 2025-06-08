9d6bf69cf4df4586065ff85c44b3bb21bbb3975c
George Castillo <4260836+gmcastil@users.noreply.github.com>
2025-06-08 08:05:52 -0600

    The initramfs archive should now include kernel modules

--- make/initramfs.mk
+++ make/initramfs.mk
@@ -18,13 +18,15 @@ INITRAMFS_SYMLINKS	:= \
 			   $(INITRAMFS_DIR)/bin/sleep \
 			   $(INITRAMFS_DIR)/bin/reboot
 
+INITRAMFS_MODULES_DEP	:= $(INITRAMFS_DIR)/lib/modules/$(KERNEL_RELEASE)/modules.dep
+
 .PHONY: initramfs initramfs-clean
 
 initramfs: $(INITRAMFS_IMAGE)
 
 # Once files and symlinks are made, we add an extra few directories and
 # then archive it all up
-$(INITRAMFS_IMAGE): $(INITRAMFS_FILES) $(INITRAMFS_SYMLINKS)
+$(INITRAMFS_IMAGE): $(INITRAMFS_FILES) $(INITRAMFS_SYMLINKS) $(INITRAMFS_MODULES_DEP)
 	$(MKDIR) -p $(INITRAMFS_DIR)/{dev,sys,proc}
 	$(MKDIR) -p $(INITRAMFS_DIR)/{mnt,tmp,var}
 	cd $(INITRAMFS_DIR) && find . | cpio -o -H newc | gzip -n > $@
@@ -41,6 +43,11 @@ $(INITRAMFS_DIR)/bin/busybox: $(BUSYBOX_BIN)
 # Kernel is going to look for init in the root directory
 $(INITRAMFS_DIR)/init: $(CONFIG_DIR)/initramfs.init
 	$(INSTALL) -m 744 $< $@
+
+# Copy kernel modules from the staging directory to the initramfs
+$(INITRAMFS_MODULES_DEP): $(KERNEL_BUILD_DIR)/.stamp-kernel-install
+	$(MKDIR) -p $(INITRAMFS_DIR)/lib/modules
+	$(CP) -av $(STAGING_DIR)/lib/modules/$(KERNEL_RELEASE) $(INITRAMFS_DIR)/lib/modules/
 
 initramfs-clean:
 	$(RM) -rf $(INITRAMFS_DIR)

39090df8b1eedd0aa1c408febd29e5b53d98f45e
George Castillo <4260836+gmcastil@users.noreply.github.com>
2025-06-10 16:51:16 -0600

    Finally fixed job control and such

--- initramfs/etc/init.d/90-login.sh
+++ initramfs/etc/init.d/90-login.sh
@@ -1,13 +1,24 @@
 #!/bin/sh
 
+# Create console device if needed
+[ -c /dev/ttyPS0 ] || mknod /dev/ttyPS0 c 5 1
+
 # Hook us up to the console before launching a shell
-exec </dev/console >/dev/console 2>&1
+exec </dev/ttyPS0 >/dev/ttyPS0 2>&1
 
 printf '%s\n' "Shell running on $(tty)" >&1
 printf '%s\n' "Type 'busybox --help' for help" >&1
 printf '\n'
 
-# Launch busybox (via sh) as a login shell, so it'll pick up /etc/profile and
-# others
-exec /bin/sh +m --login
+# Set up new session and controlling terminal
+exec setsid /bin/sh -c 'exec /bin/sh </dev/ttyPS0 >/dev/ttyPS0 2>&1 --login'
+
+# # Launch busybox (via sh) as a login shell, so it'll pick up /etc/profile and
+# # others
+# exec /bin/sh +m --login
+
+
+# # Redirect stdin/out/err to console
+# exec </dev/console >/dev/console 2>&1
+
 
